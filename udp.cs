b79142802cf2a17796fc7349ac35ee814b731133
deodell100 <deodell100@students.ecsu.edu>
2017-07-20 01:07:52 -0400

    UDP

---
+++ udp.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Text;
+using System.Threading;
+
+
+public class udp : MonoBehaviour
+{
+    UdpClient client;
+    Thread recieveThread;
+
+    // Use this for initialization
+    void Start()
+    {
+        initthread();
+    }
+
+    private void initthread()
+    {
+        recieveThread = new Thread(new ThreadStart(RecieveData));
+        recieveThread.IsBackground = true;
+        recieveThread.Start();
+        Debug.Log("Start");
+    }
+
+    private void RecieveData()
+    {
+        while (Thread.CurrentThread.IsAlive)
+        {
+            client = new UdpClient(5001);
+            IPEndPoint RemoteIpEndPoint = new IPEndPoint(IPAddress.Any, 0);
+            byte[] recieveBytes = client.Receive(ref RemoteIpEndPoint);
+            string returndata = Encoding.ASCII.GetString(recieveBytes);
+            Debug.Log(returndata);
+        }
+    }
+    // Update is called once per frame
+}
